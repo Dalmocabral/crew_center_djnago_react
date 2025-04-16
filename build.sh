77219e92a7eb17e0131311b1605773b42b038a9f
Dalmo dos Santos Cabral <dalmo.santos.cabral@gmail.com>
2025-04-16 13:39:05 -0300

    commit

--- build.sh
+++ build.sh
@@ -11,7 +11,10 @@ if [[ $CREATE_SUPERUSER ]]; then
     python manage.py shell << END
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', '123456')
+if not User.objects.filter(email='admin@example.com').exists():
+    User.objects.create_superuser(
+        email='admin@example.com',
+        password='123456'
+    )
 END
 fi
