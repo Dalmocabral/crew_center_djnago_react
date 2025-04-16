c58e814e0166e8649608ff195f29f6269d7e7237
Dalmo dos Santos Cabral <dalmo.santos.cabral@gmail.com>
2025-04-16 14:04:13 -0300

    configuracao react rende

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+'''
 if [[ $CREATE_SUPERUSER ]]; then
     python manage.py shell << END
 from django.contrib.auth import get_user_model
@@ -18,3 +19,4 @@ if not User.objects.filter(email='admin@example.com').exists():
     )
 END
 fi
+'''
