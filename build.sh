e306c1f490275d873e4de24df28c54ea372c3d1d
Dalmo dos Santos Cabral <dalmo.santos.cabral@gmail.com>
2025-04-16 13:35:29 -0300

    commit

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -o errexit
 
-cd backend  # entra na pasta onde está o manage.py
+cd backend
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser --no-input
+    python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', '123456')
+END
 fi
