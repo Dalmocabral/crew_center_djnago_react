230ecf110e8ac075dd33c449b74197fe9da59ff4
Dalmo dos Santos Cabral <dalmo.santos.cabral@gmail.com>
2025-04-16 12:16:39 -0300

    commit

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+cd Backend  # entra na pasta onde está o manage.py
 
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
