9da511a8af623100fbf3225a86546848bc78cbee
Dalmo dos Santos Cabral <dalmo.santos.cabral@gmail.com>
2025-04-16 13:23:02 -0300

    create superuser

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ cd backend  # entra na pasta onde está o manage.py
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [[$CREATE_SUPERUSER]];
+then
+    python manage.py createsuperuser --no-input
+fi
